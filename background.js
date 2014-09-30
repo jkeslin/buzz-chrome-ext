5714abc7570ac637caa6a18dca4d60df52894e43
Stephanie-Chou <stephanie.chou@outlook.com>
2014-09-30 15:26:21 -0500

    add buzzwords

--- background.js
+++ background.js
@@ -2,8 +2,14 @@
 //to check your plugin js is working uncomment the next line.
 var all = document.getElementsByTagName('p')
 
+var buzzwords = ['disrupt', 'innovate', 'innovative', 'innovation', 'space', 'vertical', 'big data', 'emerging', 'solution', 'platform', 'iterate', 'paradigm', 'passion', 'impact', 'hacking', 'change']
+
+
 for(var i = 0; i < all.length; i++){
-  var buzzParagraph = all[i].innerText.toLowerCase().replace(/the/g, 'BUZZWORD');
-  all[i].innerText = buzzParagraph;
+  for (var word = 0; word< buzzwords.length; word++){
+      var re = new RegExp(buzzwords[word],"g");
+      var buzzParagraph = all[i].innerText.toLowerCase().replace(re, 'BUZZWORD');
+      all[i].innerText = buzzParagraph;
+  }
 }
 
