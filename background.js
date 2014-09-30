c4ad0c3495fda0be359e92a553b01ed6f4a2ef95
Jeff Keslin <jkeslin@gmail.com>
2014-09-30 12:53:09 -0500

    initial buzzword replacing with single regex match

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
 //The code that is fired upon page load
 //to check your plugin js is working uncomment the next line.
-$("body").append('Test');
+var all = document.getElementsByTagName('p')
+
+for(var i = 0; i < all.length; i++){
+  var buzzParagraph = all[i].innerText.toLowerCase().replace(/the/g, 'BUZZWORD');
+  all[i].innerText = buzzParagraph;
+}
+
